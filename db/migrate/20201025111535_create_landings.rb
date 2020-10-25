b364a20fae47fa9c0d56b9f938e357dd6acb4c24
Fred Gibert <fred.gib79@gmail.com>
2020-10-25 13:12:40 +0100

    2nd commit

---
+++ db/migrate/20201025111535_create_landings.rb
@@ -0,0 +1,10 @@
+class CreateLandings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :landings do |t|
+      t.string :name
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
